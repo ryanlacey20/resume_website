69aff186c7961caceda003b126fd8de61d590128
Ryan Lacey <ryanpatricklacey@gmail.com>
2024-02-14 21:05:02 +0000

    basic functionality implemented, little to no styling

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -12,7 +12,10 @@
       </form>
       <!-- Display contact details if verification is successful -->
       <ul v-if="showContactDetails">
-        <li>Placeholder</li>
+        <li>Email: {{ email }}</li>
+        <li>Mobile: {{ phoneNumber }}</li>
+        <li>Github: {{ github }}</li>
+        <li>LinkedIn: {{ linkedin }}</li>
       </ul>
     </main>
   </div>
@@ -21,6 +24,7 @@
 <script>
 import TitleBar from './../components/TitleBar.vue';
 import axios from 'axios';
+import { ref } from 'vue';
 
 export default {
   name: 'ContactPage',
@@ -28,6 +32,10 @@ export default {
   data() {
     return {
       showContactDetails: false,
+      email: ref(''),
+      phoneNumber: ref(''),
+      github: ref(''),
+      linkedin: ref(''),
     };
   },
   mounted() {
@@ -47,7 +55,13 @@ export default {
 
         if (response.data.success) {
           // Verification successful, show contact details
+          const responseContactdetails = response.data.contactDetails
+          console.log('response data here:', responseContactdetails)
           this.showContactDetails = true;
+          this.email = responseContactdetails.email;
+          this.phoneNumber = responseContactdetails.phoneNumber;
+          this.github = responseContactdetails.github;
+          this.linkedin = responseContactdetails.linkedin;
         } else {
           // Verification failed
           console.error('Captcha verification failed', response.data);
@@ -61,10 +75,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-li {
-  list-style: none;
-  border: 1px solid black;
+#recaptcha-container {
+  justify-content: center;
 }
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 10px;
+  margin-bottom: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
 
 /* Your scoped styles */
 </style>
