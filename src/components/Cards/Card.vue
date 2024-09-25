e76d0e29382558d84840d7b030c26cbcf6d1d7c5
Ryan Lacey <ryanpatricklacey@gmail.com>
2024-09-25 01:32:50 +0100

    additional project examples added.

---
+++ src/components/Cards/Card.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    components: {},
+    created() {
+        // You can perform any setup needed here
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+    background-color: #ffffff;
+    border-radius: 15px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    height: 100%;
+    width: 70%;
+}
+</style>
--- src/views/Examples.vue
+++ src/views/Examples.vue
@@ -4,33 +4,104 @@
       <TitleBar />
     </header>
     <main>
-      <h1>Spend Smart</h1>
-      <h2>.NET - C# - Bootstrap</h2>
-      <section>
-        <p>
-          A simple app using in-browser memory to create and edit costs. Created from an introductory .NET lesson from
-          <a href="https://academy.tutorials.eu/p/csharp-progress-academy"> tutorials.eu C# academy.</a>
-        </p>
-        <a class="exampleLink" href="https://spendsmart-d3eqafhmcybxftgt.uksouth-01.azurewebsites.net/"> View Here</a>
-      </section>
+      <div class="introduction">
+        <h2>
+          Full list can be found on my GitHub:&nbsp;
+          <a href="https://github.com/ryanlacey20"> https://github.com/ryanlacey20</a>
+        </h2>
+      </div>
+      <div class="cardContainer">
+        <Card>
+          <section>
+            <h1>Spend Smart</h1>
+            <h2><img src="../../Resources/dotnetLogo.png" /> .NET - C# - Bootstrap</h2>
+
+            <p>
+              A simple app using in-browser memory to create and edit costs.
+              <br>Created from an introductory .NET lesson from
+              <a href="https://academy.tutorials.eu/p/csharp-progress-academy"> tutorials.eu C# academy.</a>
+            </p>
+            <a class="exampleLink" href="https://spendsmart-d3eqafhmcybxftgt.uksouth-01.azurewebsites.net/"> View
+              App Here</a>
+          </section>
+        </Card>
+        <Card>
+          <section>
+            <h1>Adaptive Fan</h1>
+            <h2><img src="../../Resources/arduinoLogo.png" /> C++ - Embedded Systems - Arduino</h2>
+
+            <p>
+              C++ software created for Arduino uno.
+              <br>Temperature sensor which controls a fan based on the heat levels.
+              <br>LCD connected to display the temperature in Celsius.
+            </p>
+            <a class="exampleLink" href="https://github.com/ryanlacey20/arduino-embedded-adaptive-fan-setup"> View
+              Source Code
+              Here</a>
+          </section>
+        </Card>
+        <Card>
+          <section>
+            <h1>SafeWay - Personal Safety App</h1>
+            <h2><img src="../../Resources/appleStoreLogo.jpeg" /> Mobile - iOS - Swift - Obj-C</h2>
+
+            <p>
+              iOS Personal Safety Application
+              <br> Features:
+              <br> - Panic buttons
+              <br> - Location sharing
+              <br> - Check-in features
+              <br> - Connect with the profiles of close friends and family
+            </p>
+            <a class="exampleLink" href="https://github.com/ryanlacey20/SafeWay"> View Source Code Here</a>
+          </section>
+        </Card>
+        <Card>
+          <section>
+            <h1>Resume Website</h1>
+            <h2><img src="../../Resources/vueLogo.png" /> Vue.js - Axios - JavaScript </h2>
+
+            <p>
+              This resume website used showcase brief details about the author, Ryan Lacey. <br>
+              Connects to API endpoints created by Ryan Lacey (myself) also. Back-end repo <a
+                href="https://github.com/ryanlacey20/personal_project_backends">here</a> <br>
+              Contact details hidden in environment variables behind a turing test in order to protect against web
+              scrapers.
+            </p>
+            <a class="exampleLink" href="https://github.com/ryanlacey20/resume_website"> View Source Code Here</a>
+          </section>
+        </Card>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
 import TitleBar from './../components/TitleBar.vue';
+import Card from '../components/Cards/Card.vue';
 export default {
   name: 'Examples',
-  components: { TitleBar },
-  created() {
-
-  }
+  components: { TitleBar, Card },
+  created() { }
 }
 </script>
 
 <style lang="scss" scoped>
 h1 {
   text-decoration: underline;
+  text-align: center;
+}
+
+h2 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.introduction {
+  text-align: center;
+  margin-bottom: 20px;
 }
 
 main {
@@ -38,14 +109,32 @@ main {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding-top: 20px;
+}
+
+.cardContainer {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+  width: 80%;
 }
 
 section {
-  width: 50%;
+  width: 100%;
   text-align: left;
+}
+
+.card {
+  width: 100%;
+  box-sizing: border-box;
 }
 
 .exampleLink {
   font-size: 1vw;
+}
+
+img {
+  height: 1em;
+  margin-right: 0.5em;
 }
 </style>
